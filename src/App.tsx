389ad5bc8b7c598c60b1376503b811202343420c
Mark Charles Smith <markcharlessmith@gmail.com>
2023-04-26 03:11:42 -0700

    started working on game state for App and Grid

--- src/App.tsx
+++ src/App.tsx
@@ -7,24 +7,115 @@ import Cluelist from './components/Cluelist/Cluelist'
 import { cluedata } from './cluedata'
 
 
-function App() {
-  // const [selectedSquares, setSelectedSquares] = useState([])
+// function App() {
+//   // const [selectedSquares, setSelectedSquares] = useState([])
 
-  useEffect(() => {
-  console.log(getRandomEmoji(emojidata), "random emoji")
-  }, [])
+//   useEffect(() => {
+//   console.log(getRandomEmoji(emojidata), "random emoji")
+//   }, [])
+
+//   return (
+//     <div className="App">
+//       <div className='app-container'>
+//       <h1>Emojination!</h1>
+//       <div className='game-container'>
+//         <Grid />
+//         <Cluelist clues={cluedata} />
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default App
+
+type Direction = 'horizontal' | 'vertical';
+
+type Answer = {
+  row: number;
+  col: number;
+  direction: Direction;
+  length: number;
+};
+
+type SelectedSquare = {
+  row: number;
+  col: number;
+};
+
+function App() {
+  const [grid, setGrid] = useState<string[][]>(Array(12).fill(Array(12).fill(null)));
+  const [selectedSquares, setSelectedSquares] = useState<SelectedSquare[]>([]);
+  const [correctAnswers, setCorrectAnswers] = useState<Answer[]>(generateCorrectAnswers());
+
+  function generateCorrectAnswers(): Answer[] {
+    const answers = new Set<Answer>();
+
+    while (answers.size < 9) {
+      const row = Math.floor(Math.random() * 12);
+      const col = Math.floor(Math.random() * 12);
+      const direction = Math.random() < 0.5 ? 'horizontal' : 'vertical';
+      const length = 2;
+
+      const answer: Answer = { row, col, direction, length };
+      const overlaps = answers.has(answer) || checkOverlap(answer, answers);
+
+      if (!overlaps) {
+        answers.add(answer);
+      }
+    }
+
+    return [...answers];
+  }
+
+  function checkOverlap(newAnswer: Answer, answers: Set<Answer>): boolean {
+    for (let answer of answers) {
+      if (newAnswer.direction === 'horizontal' && answer.direction === 'horizontal') {
+        if (newAnswer.row === answer.row && newAnswer.col < answer.col + answer.length && newAnswer.col + newAnswer.length > answer.col) {
+          return true;
+        }
+      } else if (newAnswer.direction === 'vertical' && answer.direction === 'vertical') {
+        if (newAnswer.col === answer.col && newAnswer.row < answer.row + answer.length && newAnswer.row + newAnswer.length > answer.row) {
+          return true;
+        }
+      } else if (newAnswer.direction === 'horizontal' && answer.direction === 'vertical') {
+        if (newAnswer.row < answer.row + answer.length && newAnswer.row + newAnswer.length > answer.row && newAnswer.col === answer.col) {
+          return true;
+        }
+      } else if (newAnswer.direction === 'vertical' && answer.direction === 'horizontal') {
+        if (newAnswer.col < answer.col + answer.length && newAnswer.col + newAnswer.length > answer.col && newAnswer.row === answer.row) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  function handleClick(row: number, col: number) {
+    const selected = { row, col };
+    const alreadySelected = selectedSquares.some(square => square.row === row && square.col === col);
+
+    if (!alreadySelected) {
+      setSelectedSquares([...selectedSquares, selected]);
+    } else {
+      setSelectedSquares(selectedSquares.filter(square => !(square.row === row && square.col === col)));
+    }
+  }
 
   return (
-    <div className="App">
-      <div className='app-container'>
-      <h1>Emojination!</h1>
-      <div className='game-container'>
-        <Grid />
-        <Cluelist clues={cluedata} />
-        </div>
-      </div>
+    <div className="app">
+      <h1>Crossword Puzzle</h1>
+      <Grid
+        grid={grid}
+        selectedSquares={selectedSquares}
+        correctAnswers={correctAnswers}
+        clues={cluedata}
+        onSquareClick={handleClick}
+      />
+      <Cluelist clues={cluedata} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -2,16 +2,45 @@ import React from 'react';
 import './grid.css';
 import { getRandomEmoji } from '../../utils';
 import { emojidata } from '../../emojidata';
+import { cluedata } from '../../cluedata';
 
-const Grid: React.FC = () => {
-  const rows = Array(12).fill(null);
-  const cols = Array(12).fill(null);
+// const Grid: React.FC = () => {
+//   const rows = Array(12).fill(null);
+//   const cols = Array(12).fill(null);
   
+//   return (
+//     <div className="grid">
+//       {rows.map((_, rowIndex) => (
+//         cols.map((_, colIndex) => (
+//           <div key={`${rowIndex}-${colIndex}`} className="square">{getRandomEmoji(emojidata).symbol}</div>
+//         ))
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default Grid;
+
+type GridProps = {
+  grid: string[][],
+  selectedSquares: { row: number, col: number }[],
+  correctAnswers: { row: number, col: number, direction: string, length: number }[],
+  clues: { key: string, text: string, theme: string, emojiKeys: string[] }[],
+  onSquareClick: (row: number, col: number) => void
+}
+
+const Grid: React.FC<GridProps> = ({ grid, selectedSquares, correctAnswers, clues, onSquareClick }) => {
   return (
     <div className="grid">
-      {rows.map((_, rowIndex) => (
-        cols.map((_, colIndex) => (
-          <div key={`${rowIndex}-${colIndex}`} className="square">{getRandomEmoji(emojidata).symbol}</div>
+      {grid.map((row, rowIndex) => (
+        row.map((col, colIndex) => (
+          <div 
+            key={`${rowIndex}-${colIndex}`} 
+            className={`square ${selectedSquares.some(square => square.row === rowIndex && square.col === colIndex) ? 'selected' : ''}`}
+            onClick={() => onSquareClick(rowIndex, colIndex)}
+          >
+            {grid[rowIndex][colIndex] ? grid[rowIndex][colIndex] : getRandomEmoji(emojidata).symbol}
+          </div>
         ))
       ))}
     </div>
